e828e8a99ec4cddcd45ae1b45cdd02e74ff53985
Scott Gibb <scott.gibb@dyson.com>
2024-11-07 14:38:19 +0000

    update sh

--- ci.sh
+++ ci.sh
@@ -7,12 +7,8 @@ export RUSTFLAGS=-Dwarnings
 cargo +nightly fmt -- --check
 
 cargo clippy
-cargo clippy --features embassy-time
 
 cargo clippy --features defmt
-cargo clippy --features defmt,embassy-time
 
 cargo clippy --features log
-cargo clippy --features log,embassy-time
 
-cargo test --features embassy-time
